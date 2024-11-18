341660a8097f8762cfabfe4454eb5af1deee171a
Ian Ndiga <ianndiga0@gmail.com>
2024-11-18 14:45:32 +0300

    added

---
+++ week 3.py
@@ -0,0 +1,19 @@
+def calculate_discount(price, discount_percent):
+    # Check if the discount is 20% or higher
+    if discount_percent >= 20:
+        discount_amount = (discount_percent / 100) * price
+        final_price = price - discount_amount
+        return final_price
+    else:
+        return price
+try:
+    original_price = float(input("Enter the original price of the item: $"))
+    discount_percent = float(input("Enter the discount percentage: "))
+    final_price = calculate_discount(original_price, discount_percent)
+    if final_price == original_price:
+        print(f"The original price is: ${original_price:.2f}")
+    else:
+        print(f"The final price after applying the discount is: ${final_price:.2f}")
+
+except ValueError:
+    print("Please enter valid numeric values.")
