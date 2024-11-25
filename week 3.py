199ed3c4f141878a09fb7554019ee489cd530a92
Ian Ndiga <ianndiga0@gmail.com>
2024-11-25 15:14:14 +0300

    added

--- week 3.py
+++ week 3.py
@@ -1,5 +1,4 @@
 def calculate_discount(price, discount_percent):
-    # Check if the discount is 20% or higher
     if discount_percent >= 20:
         discount_amount = (discount_percent / 100) * price
         final_price = price - discount_amount
